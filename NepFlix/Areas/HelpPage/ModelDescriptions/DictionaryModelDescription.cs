fff191b3a7b6dd160c3e9dcc213e68a714f9a391
HansVoorneveld <hans.voorneveld@co3logie.com>
2015-12-14 01:10:59 +0100

    1st

---
+++ NepFlix/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace NepFlix.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ NepFlix/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace NepFlix.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
